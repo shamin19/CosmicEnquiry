6e50cfe83721d60cb34e0b93878579e738c602d9
shamin19 <168615431+shamin19@users.noreply.github.com>
2024-05-15 14:00:13 +0800

    auth.py

---
+++ auth.py
@@ -0,0 +1,17 @@
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def authenticate_and_create_drive():
+    # Authenticate and create the PyDrive client.
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("mycreds.txt")
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+    gauth.SaveCredentialsFile("mycreds.txt")
+    drive = GoogleDrive(gauth)
+    return drive
